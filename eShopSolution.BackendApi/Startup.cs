332189e28f8a89a7a372af83bd298d535a6db4e7
Le Thong <hichita1324@yahoo.com>
2021-08-04 15:01:39 +0700

    Add Swagger

--- eShopSolution.BackendApi/Startup.cs
+++ eShopSolution.BackendApi/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,6 +32,9 @@ namespace eShopSolution.BackendApi
         options.UseSqlServer(Configuration.GetConnectionString(SystemConstants.MainConnectionString)));
             // Declare DI
             services.AddTransient<IPublicProductService, PublicProductService>();
+            services.AddSwaggerGen(c=> {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Swagger eShopSolution", Version = "v1" });
+            });
             services.AddControllersWithViews();
         }
 
@@ -49,7 +53,15 @@ namespace eShopSolution.BackendApi
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
 
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger eShopSolution V1");
+            });
             app.UseRouting();
 
             app.UseAuthorization();
