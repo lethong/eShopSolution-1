5f72b65404a1f9ce4b57684dba2a287af1849422
Le Thong <hichita1324@yahoo.com>
2021-08-30 23:10:25 +0700

    Create controller upload image ck editor

---
+++ eShopSolution.AdminApp/Controllers/UploadImageController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eShopSolution.AdminApp.Controllers
+{
+    public class UploadImageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<JsonResult> OnPostUploadImage([FromForm] IFormFile upload)
+        {
+            if (upload.Length <= 0) return null;
+
+            //your custom code logic here
+
+            //1)check if the file is image
+
+            //2)check if the file is too large
+
+            //etc
+
+            var fileName = Guid.NewGuid() + Path.GetExtension(upload.FileName).ToLower();
+
+            //save file under wwwroot/CKEditorImages folder
+
+            var filePath = Path.Combine(
+                Directory.GetCurrentDirectory(), "wwwroot/CKEditorImages",
+                fileName);
+
+            using (var stream = System.IO.File.Create(filePath))
+            {
+                await upload.CopyToAsync(stream);
+            }
+
+            var url = $"{"/CKEditorImages/"}{fileName}";
+
+            var success = new uploadsuccess
+            {
+                Uploaded = 1,
+                FileName = fileName,
+                Url = url
+            };
+
+            return new JsonResult(success);
+        }
+
+        public class uploadsuccess
+        {
+            public int Uploaded { get; set; }
+            public string FileName { get; set; }
+            public string Url { get; set; }
+        }
+    }
+}
